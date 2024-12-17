1df4ad0150541402efdf8ccaeaba6a4495fdeacf
IanKilwiny <ribeiroian083@gmail.com>
2024-12-17 14:26:04 -0300

    Adicionado o insertion sort otimizado

--- src/App.java
+++ src/App.java
@@ -1,12 +1,12 @@
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.List;
+
 import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
         Random rand = new Random();
-        int tam = 100000;
+        int tam = 10000;
 
         Integer[] nums = new Integer[tam];
 
@@ -14,13 +14,19 @@ public class App {
             nums[i] = rand.nextInt(1000);
         }
 
-        medindoBubbleSortSimples(nums);
+        medindoBubbleSortSimples(nums); //  5 segundos
+        // medindoBubbleSortOtimizado(nums); // 8 segundos 1 s
+
+        // Integer nums[] = {9,8,7,6,5,4,3,2,1};
+        // InsertionSort insertionSort = new InsertionSort();
+        // System.out.println("Desordenado: "+Arrays.asList(nums));
+        // System.out.println("Ordenado: "+Arrays.asList(insertionSort.insertionSortOtimizado(nums)));
 
 
     }
 
     public static void medindoBubbleSortSimples(Integer[] nums){
-        System.out.println("#######BUBBLE SORT SIMPLES############");
+        // System.out.println("#######BUBBLE SORT SIMPLES############");
         BubbleSort bubbleSort = new BubbleSort();
         // System.out.println("Desordenado: "+Arrays.asList(nums));
         System.out.println("TEMPO INICIAL: "+LocalTime.now().format(DateTimeFormatter.ofPattern("mm:ss")));
@@ -30,9 +36,14 @@ public class App {
         System.out.println("TEMPO FINAL: "+LocalTime.now().format(DateTimeFormatter.ofPattern("mm:ss")));
         // System.out.println(Arrays.asList(ordenacao));
 
+      
+    }
+
+    public static void medindoBubbleSortOtimizado(Integer[] nums) {
+        BubbleSort bubbleSort = new BubbleSort();
         System.out.println("###########BUBBLE SORT OTIMIZADO##########");
         System.out.println("TEMPO INICIAL: "+LocalTime.now().format(DateTimeFormatter.ofPattern("mm:ss")));
-        ordenacao = bubbleSort.bubleSortOtimizado(nums);
+        Integer[] ordenacao = bubbleSort.bubleSortOtimizado(nums);
         System.out.println("TEMPO FINAL: "+LocalTime.now().format(DateTimeFormatter.ofPattern("mm:ss")));
     }
 
--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -15,14 +15,39 @@ public class InsertionSort {
         }
         return nums;
     }
+
+    public Integer[] insertionSortOtimizado(Integer[] nums) {
+       
+        for (int i = 1; i < nums.length; i++) {
+            int alvo = nums[i];  //salva o 4
+            int pos = buscaBinaria(nums, 0, i - 1, alvo);  //
+            
+            
+            for (int j = i; j > pos; j--) {
+                nums[j] = nums[j - 1];
+            }
+            
+            nums[pos] = alvo;
+        }
+        
+        return nums;
+    }
+    
+    private int buscaBinaria(Integer[] nums, int inicio, int fim, int alvo) {
+      
+        while (inicio <= fim) {
+           int meio = (inicio + fim) / 2;
+            
+            if (nums[meio] == alvo) {
+                return meio;  
+            } else if (nums[meio] > alvo) {
+                fim = meio - 1;  
+            } else {
+                inicio = meio + 1;  
+            }
+        }
+        
+        return inicio;  
+    }
 }
 
-/*aux = 2
- * 3 2 1
- * 3 3 1
- * 2 3 1
- * aux = 1
- * 2 3 3
- * 2 2 3
- * 1 2 3
- */
